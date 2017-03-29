f6b3e585b5bceb8a148e08504c074dda915439cc
Dmitry Chirikov <dmitry@chirikov.ru>
2017-03-30 01:38:32 +0200

    Rework test exports. Add filters for tests.

--- tests/suite.py
+++ tests/suite.py
@@ -1,34 +1,48 @@
 import unittest
-
-import test_cluster as cluster
-import test_network as network
-import test_bmcsetup as bmcsetup
-import test_utils_ip as utils_ip
-import test_utils_freelist as utils_freelist
-import test_osimage as osimage
-import test_group as group
-
+import os
 from optparse import OptionParser
 
+"""
+to run tests:
+
+    All tests
+        $ tox
+
+    All tests in all files matching glob
+        $ tox --  -p *node*
+
+    All tests in class in module:
+        $ tox -- test_node.NodeCreateTests
+
+    Single test:
+        $ tox -- test_node.NodeCreateTests.test_create_named_node
+"""
 
 parser = OptionParser('usage: %prog [options] -- [testsuite options]')
+
 parser.add_option('-v', '--verbose',
                   action='count', dest='verbose', default=1,
                   help='increase verbosity')
+
+parser.add_option('-p', '--pattern',
+                  dest='pattern',
+                  default='test*.py',
+                  help='pattern for tests')
 
 (options, args) = parser.parse_args()
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
-suite.addTests(loader.loadTestsFromModule(utils_ip))
-suite.addTests(loader.loadTestsFromModule(utils_freelist))
-suite.addTests(loader.loadTestsFromModule(cluster))
-suite.addTests(loader.loadTestsFromModule(network))
-suite.addTests(loader.loadTestsFromModule(bmcsetup))
-suite.addTests(loader.loadTestsFromModule(osimage))
-suite.addTests(loader.loadTestsFromModule(group))
+if args:
+    for elem in args:
+        suite.addTests(loader.loadTestsFromName(elem))
+else:
+    tests_dir = os.path.dirname(os.path.realpath(__file__))
+    suite.addTests(loader.discover(tests_dir, options.pattern))
 
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner(verbosity=options.verbose)
+    runner = unittest.TextTestRunner(
+        verbosity=options.verbose,
+    )
     runner.run(suite)
