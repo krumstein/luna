85534d547a52609f71c5c9c3d6ccbd796e26428d
Dmitry Chirikov <dmitry@chirikov.ru>
2017-03-30 01:28:35 +0200

    Move mock_osimage_tree and create_luna_homedir to Sandbox class

--- tests/helper_utils.py
+++ tests/helper_utils.py
@@ -6,10 +6,51 @@ import time
 import tempfile
 import pymongo
 import subprocess
+import ming
+import uuid
 
 
-specfile_template = """
-Name:                   kernel
+class Sandbox(object):
+    """
+    Class for creating sanboxed envidonment for Luna
+    Class.db     - MongoDB or Ming database.
+                   Depends if mongod executable exist.
+    Class.dbtype - Current DB.
+    Class.path   - Path to temporary folder.
+    """
+
+    def __init__(self, path = None, dbtype = 'auto'):
+
+        if not path:
+            self.path = tempfile.mkdtemp(prefix='luna')
+        else:
+            # can cause race condition, but ok
+            if not os.path.exists(path):
+                os.makedirs(path)
+            self.path = path
+        self._dbconn = None
+        self._mongopath = self.path + "/mongo"
+        if not os.path.exists(self._mongopath):
+            os.makedirs(self._mongopath)
+        self._mongoprocess = None
+        if dbtype == 'auto':
+            try:
+                self._start_mongo()
+                self.dbtype = 'mongo'
+            except OSError:
+                self._mingdatastore = ming.create_datastore('mim:///' + str(uuid.uuid4()))
+                self._dbconn = self._mingdatastore.db.luna
+                self.dbtype = 'ming'
+        elif dbtype == 'mongo':
+            self._start_mongo()
+            self.dbtype = 'mongo'
+        else:
+            self._mingdatastore = ming.create_datastore('mim:///' + str(uuid.uuid4()))
+            self._dbconn = self._mingdatastore.db.luna
+            self.dbtype = 'ming'
+        self._create_luna_homedir()
+
+    _specfile_template = """Name:                   kernel
 Version:                {version}
 Release:                {release}
 Vendor:                 dummy
@@ -23,135 +64,116 @@ Provides:               kernel
 %description
 %{{summary}}
 
-%files
-"""
-def mock_osimage_tree(osimage_path, kern_versions = ['1.0.0']):
+%files"""
 
-    rpmtsCallback_fd = None
+    def create_osimage(self, kern_versions = ['1.0.0']):
 
-    def runCallback(reason, amount, total, key, client_data):
-        global rpmtsCallback_fd
-        if reason == rpm.RPMCALLBACK_INST_OPEN_FILE:
-            rpmtsCallback_fd = os.open(key, os.O_RDONLY)
-            return rpmtsCallback_fd
-        elif reason == rpm.RPMCALLBACK_INST_START:
-            os.close(rpmtsCallback_fd)
+        rpmtsCallback_fd = None
 
-    # create rpmdb tree
-    rpmdb_path = osimage_path + '/var/lib/rpm'
-    if not os.path.exists(rpmdb_path):
-        os.makedirs(rpmdb_path)
+        def runCallback(reason, amount, total, key, client_data):
+            global rpmtsCallback_fd
+            if reason == rpm.RPMCALLBACK_INST_OPEN_FILE:
+                rpmtsCallback_fd = os.open(key, os.O_RDONLY)
+                return rpmtsCallback_fd
+            elif reason == rpm.RPMCALLBACK_INST_START:
+                os.close(rpmtsCallback_fd)
+        
+        # create dir for osimage
+        if not os.path.exists(self.path + '/os'):
+            os.makedirs(self.path + '/os')
+        osimage_path = tempfile.mkdtemp(
+            prefix = 'os',
+            dir = self.path + '/os/'
+        )
+        # create rpmdb tree
+        rpmdb_path = osimage_path + '/var/lib/rpm'
+        if not os.path.exists(rpmdb_path):
+            os.makedirs(rpmdb_path)
 
-    # initialize rpmdb
-    rpm.addMacro('_dbpath', rpmdb_path)
-    ts = rpm.TransactionSet()
-    ts.initDB()
-
-    # create rpmbuild endironment
-    rpmbuild_path = osimage_path + '/rpmbuild'
-    if not os.path.exists(rpmbuild_path):
-        for subdir in ['BUILD',  'BUILDROOT', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS']:
-            d = rpmbuild_path + '/' + subdir
-            if not os.path.exists(d):
-                os.makedirs(d)
-
-    # build and install fake kernel packages
-    for kern_ver in kern_versions:
-
-        #
-        # create spec file
-        #
-
-        kern_release = '1.el7'
-        spec_content = specfile_template.format(version = kern_ver, release = kern_release)
-        spec_file_name = rpmbuild_path + '/SPECS/kernel-' + kern_ver + '.spec'
-        with open(spec_file_name, 'w') as fd:
-            fd.write(spec_content)
-
-        #
-        # build rpm
-        #
-
-        rpmbuild_cmd = ['rpmbuild', '--define', '"_topdir ' + rpmbuild_path +'"',
-                '-ba', spec_file_name]
-
-        # Suppressing all outputs. Those are annoing, when running it million times.
-        devnull = open(os.devnull, 'w')
-        res = subprocess.call(" ".join(rpmbuild_cmd), shell=True, stdout=devnull, stderr=devnull)
-        if res:
-            print "ERROR: Unable to build rpm"
-            raise RuntimeError
-        #
-        # 'install' rpms
-        #
-
+        # initialize rpmdb
+        rpm.addMacro('_dbpath', rpmdb_path)
         ts = rpm.TransactionSet()
-        # do dot verify DSA signatures
-        ts.setVSFlags(-1)
-        # rpm --justdb
-        ts.setFlags(rpm.RPMTRANS_FLAG_JUSTDB)
+        ts.initDB()
 
-        rpm_file_name = (rpmbuild_path
-                + '/RPMS/x86_64/kernel-'
-                + kern_ver + '-' + kern_release
-                + '.x86_64.rpm'
-            )
-        rpm_header = None
-        # read rpm header
-        with open(rpm_file_name, 'r') as fd:
-            rpm_header = ts.hdrFromFdno(fd)
-        # add rpm to transaction
-        ts.addInstall(rpm_header, rpm_file_name, 'i')
+        # create rpmbuild endironment
+        rpmbuild_path = osimage_path + '/rpmbuild'
+        if not os.path.exists(rpmbuild_path):
+            for subdir in ['BUILD',  'BUILDROOT', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS']:
+                d = rpmbuild_path + '/' + subdir
+                if not os.path.exists(d):
+                    os.makedirs(d)
 
-        # finally perform install
-        ts.run(runCallback, 1)
+        # build and install fake kernel packages
+        for kern_ver in kern_versions:
 
-        return True
+            #
+            # create spec file
+            #
 
-def create_luna_homedir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if 'VIRTUAL_ENV' not in os.environ:
-        print "Runinning not in virtual environment"
-        virtual_env_path = '/'
-    else:
-        virtual_env_path = os.environ['VIRTUAL_ENV']
-    for d in ['boot', 'torrents']:
-        if not os.path.exists(path + '/' + d):
-            os.makedirs(path + '/' + d)
-    source_template_path = virtual_env_path + '/usr/share/luna/templates'
-    if not os.path.exists(path + '/templates'):
-        shutil.copytree(source_template_path, path + '/templates')
+            kern_release = '1.el7'
+            spec_content = self._specfile_template.format(version = kern_ver, release = kern_release)
+            spec_file_name = rpmbuild_path + '/SPECS/kernel-' + kern_ver + '.spec'
+            with open(spec_file_name, 'w') as fd:
+                fd.write(spec_content)
 
-class sandbox(object):
+            #
+            # build rpm
+            #
 
-    def __init__(self, path = None):
+            rpmbuild_cmd = ['rpmbuild', '--define', '"_topdir ' + rpmbuild_path +'"',
+                    '-ba', spec_file_name]
 
-        if not path:
-            self.path = tempfile.mkdtemp()
+            # Suppressing all outputs. Those are annoing, when running it million times.
+            devnull = open(os.devnull, 'w')
+            res = subprocess.call(" ".join(rpmbuild_cmd), shell=True, stdout=devnull, stderr=devnull)
+            if res:
+                assert False, "ERROR: Unable to build rpm"
+            #
+            # 'install' rpms
+            #
+
+            ts = rpm.TransactionSet()
+            # do dot verify DSA signatures
+            ts.setVSFlags(-1)
+            # rpm --justdb
+            ts.setFlags(rpm.RPMTRANS_FLAG_JUSTDB)
+
+            rpm_file_name = (rpmbuild_path
+                    + '/RPMS/x86_64/kernel-'
+                    + kern_ver + '-' + kern_release
+                    + '.x86_64.rpm'
+                )
+            rpm_header = None
+            # read rpm header
+            with open(rpm_file_name, 'r') as fd:
+                rpm_header = ts.hdrFromFdno(fd)
+            # add rpm to transaction
+            ts.addInstall(rpm_header, rpm_file_name, 'i')
+
+            # finally perform install
+            ts.run(runCallback, 1)
+
+            return osimage_path
+
+    def _create_luna_homedir(self):
+        """
+        Creates ./boot, ./torrents, ./templates
+        subdirs in path
+        """
+
+        if 'VIRTUAL_ENV' not in os.environ:
+            print "Runinning not in virtual environment"
+            virtual_env_path = '/'
         else:
-            # can cause race condition, but ok
-            if not os.path.exists(path):
-                os.makedirs(path)
-            self.path = path
-        self._dbconn = None
-        # TODO
-        # check if mongod binary exists
-        self._mongopath = self.path + "/mongo"
-        if not os.path.exists(self._mongopath):
-            os.makedirs(self._mongopath)
-        self._mongoprocess = None
-        try:
-            self._start_mongo(
-            self.dbtype = 'mongo'
-        except OSError:
-            import ming
-            self._mingdatastore = ming.create_datastore('mim:///luna')
-            self._dbconn = self._mingdatastore.db.luna
-            self.dbtype = 'ming'
-            
+            virtual_env_path = os.environ['VIRTUAL_ENV']
+        for d in ['boot', 'torrents']:
+            if not os.path.exists(self.path + '/' + d):
+                os.makedirs(self.path + '/' + d)
+        source_template_path = virtual_env_path + '/usr/share/luna/templates'
+        if not os.path.exists(self.path + '/templates'):
+            shutil.copytree(source_template_path, self.path + '/templates')
+
     def _start_mongo(self):
-        self._mongoprocess = None
         # will try 5 times
         for t in range(5):
             s = socket.socket()
@@ -159,7 +181,10 @@ class sandbox(object):
             dbport = s.getsockname()[1]
             s.close()
             s = None
-            self._mongoprocess = subprocess.Popen(['mongod', '--bind_ip', 'localhost',
+            self._mongoprocess = subprocess.Popen([
+                'mongod',
+                '--bind_ip',
+                'localhost',
                 '--port', str(dbport),
                 '--dbpath', self._mongopath,
                 '--nojournal', '--nohttpinterface',
@@ -171,14 +196,14 @@ class sandbox(object):
                 stderr=subprocess.STDOUT
 
             )
-            print dbport
+
+            # it might take some time to bring DB up
 
             for i in range(3):
                 time.sleep(1)
                 try:
                     self._dbconn = pymongo.MongoClient('localhost:'+ str(dbport))['luna']
                 except pymongo.errors.ConnectionFailure:
-                    print "+++"
                     continue
                 else:
                     break
