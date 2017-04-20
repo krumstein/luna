e2b7b921a25575df8c234caa42d8a80310d6f909
Dmitry Chirikov <dmitry@chirikov.ru>
2017-04-20 21:55:16 +0200

    IPv6 tests for utils.ip

--- tests/test_utils_ip.py
+++ tests/test_utils_ip.py
@@ -1,11 +1,13 @@
 from mock import patch
 import unittest
 
-import os
 from luna.utils import ip
 
 
-class UtilsIPTests(unittest.TestCase):
+class UtilsIPTestsV4(unittest.TestCase):
+
+    def setUp(self):
+        print
 
     def test_aton_with_valid_ip(self):
         self.assertEqual(ip.aton('10.0.0.1'), 167772161)
@@ -64,6 +66,58 @@ class UtilsIPTests(unittest.TestCase):
 
         self.assertEqual(ip.guess_ns_hostname(), 'controller')
 
+
+class UtilsIPTestsV6(unittest.TestCase):
+
+    def setUp(self):
+        print
+
+    def test_aton_with_valid_ip(self):
+        self.assertEqual(
+            ip.aton('fd12:3456:789a:1::1', 6),
+            336389205813283084628800618700287770625
+        )
+
+    def test_aton_with_invalid_format(self):
+        self.assertRaises(RuntimeError, ip.aton, 'fd12:3456:789a::1::1')
+
+    def test_aton_with_invalid_ip(self):
+        self.assertRaises(RuntimeError, ip.aton, 'gd12:3456:789a:1::1')
+
+    def test_ntoa_with_valid_absnum(self):
+        self.assertEqual(
+            ip.ntoa(336389205813283084628800618700287770625, 6),
+            'fd12:3456:789a:1::1'
+        )
+
+    def test_ntoa_with_invalid_absnum(self):
+        self.assertRaises(
+            RuntimeError,
+            ip.ntoa,
+            340282366920938463463374607431768211456,
+            6
+        )
+
+    def test_get_num_subnet_with_valid_input(self):
+        num_subnet = ip.get_num_subnet('fd12:3456:789a:1::1', 64, 6)
+        self.assertEqual(
+            num_subnet,
+            336389205813283084628800618700287770624
+        )
+
+    def test_get_num_subnet_with_invalid_prefix(self):
+        self.assertRaises(
+            RuntimeError,
+            ip.get_num_subnet,
+            'fd12:3456:789a:1::1', 129, 6
+        )
+
+    def test_get_num_subnet_with_invalid_address(self):
+        self.assertRaises(
+            RuntimeError,
+            ip.get_num_subnet,
+            'gd12:3456:789a:1::1', 64, 6
+        )
 
 if __name__ == '__main__':
     unittest.main()
