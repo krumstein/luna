d71880992e69dec13fdab82d141de54cff950902
Dmitry Chirikov <dmitry@chirikov.ru>
2017-04-24 13:47:03 +0200

    IPv6 support for Network() class

--- luna/network.py
+++ luna/network.py
@@ -19,8 +19,7 @@ You should have received a copy of the GNU General Public License
 along with Luna.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
-
-from config import *
+from config import usedby_key
 
 import logging
 
@@ -36,8 +35,10 @@ class Network(Base):
 
     log = logging.getLogger(__name__)
 
-    def __init__(self, name=None, mongo_db=None, create=False, id=None,
-                 NETWORK=None, PREFIX=None, ns_hostname=None, ns_ip=None):
+    def __init__(self, name=None, mongo_db=None,
+                 create=False, id=None, version=4,
+                 NETWORK=None, PREFIX=None,
+                 ns_hostname=None, ns_ip=None):
         """
         create  - should be True if we need create a network
         NETWORK - network
@@ -50,7 +51,7 @@ class Network(Base):
 
         self._collection_name = 'network'
         self._keylist = {'NETWORK': long, 'PREFIX': int,
-                         'ns_hostname': type(''), 'ns_ip': long}
+                         'ns_hostname': type('')}
 
         # Check if this network is already present in the datastore
         # Read it if that is the case
@@ -58,9 +59,24 @@ class Network(Base):
         net = self._get_object(name, mongo_db, create, id)
 
         if create:
+            if version not in [4, 6]:
+                self.log.error("version should be 4 or 6")
+                raise RuntimeError
+
+            maxbits = 32
+            if version == 6:
+                maxbits = 128
+
+            self.version = version
+            self.maxbits = maxbits
+
             cluster = Cluster(mongo_db=self._mongo_db)
-            num_subnet = utils.ip.get_num_subnet(NETWORK, PREFIX)
-            flist = [{'start': 1, 'end': (1 << (32 - int(PREFIX))) - 2}]
+            num_subnet = utils.ip.get_num_subnet(NETWORK, PREFIX, self.version)
+
+            flist = self._flist_to_str([{
+                'start': 1,
+                'end': (1 << (self.maxbits - int(PREFIX))) - 2
+            }])
 
             # Try to guess the nameserver hostname if none provided
 
@@ -68,14 +84,17 @@ class Network(Base):
                 ns_hostname = utils.ip.guess_ns_hostname()
 
             # Store the new network in the datastore
+            if self.version == 6:
+                num_subnet = str(num_subnet)
 
             net = {'name': name, 'NETWORK': num_subnet, 'PREFIX': PREFIX,
                    'freelist': flist, 'ns_hostname': ns_hostname,
-                   'ns_ip': None}
+                   'ns_ip': None, 'version': version}
 
             self.log.debug("Saving net '{}' to the datastore".format(net))
 
             self.store(net)
+            self._convert_to_int()
 
             # Link this network to the current cluster
 
@@ -85,36 +104,98 @@ class Network(Base):
             # the cluster's frontend address
 
             if ns_ip is None:
-                ns_ip = utils.ip.reltoa(num_subnet, flist[0]['end'])
+                ns_ip = str(utils.ip.reltoa(num_subnet,
+                                            int(flist[0]['end']),
+                                            self.version))
 
             self.set('ns_ip', ns_ip)
 
+        self.version = self._json['version']
+        self.maxbits = 32
+        if self.version == 6:
+            self.maxbits = 128
+        self._convert_to_int()
+
         self.log = logging.getLogger(__name__ + '.' + self._name)
+
+    def _convert_to_int(self):
+        """
+        Converting self._json from strings to integers
+        This is required, as mongo is unable to sore int representation
+        of IPv6
+        """
+
+        if self.version == 4:
+            return None
+
+        self._json['NETWORK'] = int(self._json['NETWORK'])
+        flist = []
+        for elem in self._json['freelist']:
+            start = elem['start']
+            end = elem['end']
+            flist.append({
+                'start': int(start),
+                'end': int(end)
+            })
+        self._json['freelist'] = flist
+
+    def _nsip_to_str(self, flist):
+        """
+        Convert ns_ip to strings for IPv6
+        """
+        if self.version == 4:
+            return flist
+        return str(flist)
+
+    def _flist_to_str(self, flist):
+        """
+        Convert flist's interegers to strings for IPv6
+        """
+        if self.version == 4:
+            return flist
+        new_flist = []
+        for elem in flist:
+            new_elem = {
+                'start': str(elem['start']),
+                'end': str(elem['end'])
+            }
+            new_flist.append(new_elem)
+        return new_flist
 
     def set(self, key, value):
         net = self._json
 
         if key == 'ns_ip':
-            rel_ns_ip = utils.ip.atorel(value, net['NETWORK'], net['PREFIX'])
+
+            rel_ns_ip = utils.ip.atorel(
+                value, net['NETWORK'], net['PREFIX'], self.version
+            )
+
             old_ip = net['ns_ip']
 
             if old_ip:
                 self.release_ip(old_ip)
 
             self.reserve_ip(rel_ns_ip)
-            ret = super(Network, self).set('ns_ip', rel_ns_ip)
+            ret = super(Network, self).set(
+                'ns_ip', self._nsip_to_str(rel_ns_ip)
+            )
 
         elif key == 'NETWORK':
             prefix = net['PREFIX']
-            num_subnet = utils.ip.get_num_subnet(value, prefix)
+            num_subnet = utils.ip.get_num_subnet(
+                value, prefix, self.version
+            )
 
-            ret = super(Network, self).set('NETWORK', num_subnet)
+            ret = super(Network, self).set('NETWORK', str(num_subnet))
 
         elif key == 'PREFIX':
             num_subnet = net['NETWORK']
-            new_num_subnet = utils.ip.get_num_subnet(num_subnet, value)
+            new_num_subnet = utils.ip.get_num_subnet(
+                num_subnet, value, self.version
+            )
 
-            limit = (1 << (32 - value)) - 1
+            limit = (1 << (self.maxbits - value)) - 1
             flist = utils.freelist.set_upper_limit(net['freelist'], limit)
 
             ret = super(Network, self).set('freelist', flist)
@@ -130,16 +211,24 @@ class Network(Base):
         net = self._json
 
         if key == 'NETWORK':
-            value = utils.ip.ntoa(net[key])
+            value = utils.ip.ntoa(
+                net[key], self.version
+            )
 
         elif key == 'NETMASK':
             prefix = int(net['PREFIX'])
-            num_mask = ((1 << 32) - 1) ^ ((1 << (33 - prefix) - 1) - 1)
+            num_mask = (((1 << self.maxbits) - 1)
+                        ^ ((1 << (self.maxbits+1 - prefix) - 1) - 1)
+                        )
 
-            value = utils.ip.ntoa(num_mask)
+            value = utils.ip.ntoa(
+                num_mask, self.version
+            )
 
         elif key == 'ns_ip':
-            value = utils.ip.reltoa(net['NETWORK'], net['ns_ip'])
+            value = utils.ip.reltoa(
+                net['NETWORK'], net['ns_ip'], self.version
+            )
 
         else:
             value = super(Network, self).get(key)
@@ -150,10 +239,14 @@ class Network(Base):
         net = self._json
 
         if type(ip1) is str:
-            ip1 = utils.ip.atorel(ip1, net['NETWORK'], net['PREFIX'])
+            ip1 = utils.ip.atorel(
+                ip1, net['NETWORK'], net['PREFIX'], self.version
+            )
 
         if type(ip2) is str:
-            ip2 = utils.ip.atorel(ip2, net['NETWORK'], net['PREFIX'])
+            ip2 = utils.ip.atorel(
+                ip2, net['NETWORK'], net['PREFIX'], self.version
+            )
 
         if bool(ip2) and ip2 <= ip1:
             self.log.error("Wrong range definition.")
@@ -166,25 +259,30 @@ class Network(Base):
         elif ignore_errors:
             flist, unfreed = utils.freelist.next_free(net['freelist'])
 
-        self.set('freelist', flist)
+        self.set('freelist', self._flist_to_str(flist))
 
         return unfreed
 
     def release_ip(self, ip1, ip2=None):
         net = self._json
+        self._convert_to_int()
 
         if type(ip1) is str:
-            ip1 = utils.ip.atorel(ip1, net['NETWORK'], net['PREFIX'])
+            ip1 = utils.ip.atorel(
+                ip1, net['NETWORK'], net['PREFIX'], self.version
+            )
 
         if type(ip2) is str:
-            ip2 = utils.ip.atorel(ip2, net['NETWORK'], net['PREFIX'])
+            ip2 = utils.ip.atorel(
+                ip2, net['NETWORK'], net['PREFIX'], self.version
+            )
 
         if bool(ip2) and ip2 <= ip1:
             self.log.error("Wrong range definition.")
             return None
 
         flist, freed = utils.freelist.free_range(net['freelist'], ip1, ip2)
-        self.set('freelist', flist)
+        self.set('freelist', self._flist_to_str(flist))
 
         return True
 
@@ -210,7 +308,9 @@ class Network(Base):
                 self.log.error(("Duplicate name '{}' in network '{}'"
                                 .format(name, self.name)))
             except:
-                out_dict[name] = utils.ip.reltoa(net['NETWORK'], relative_ip)
+                out_dict[name] = utils.ip.reltoa(
+                    net['NETWORK'], relative_ip, self.version
+                )
 
         for elem in rev_links:
             if elem == "group":
