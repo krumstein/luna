9f40b57590253d11c368c17b6fa2ba63f1a95002
Dmitry Chirikov <dmitry@chirikov.ru>
2017-08-17 19:02:16 +0200

    Use relative paths and build binary rpm

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
-if  ! git describe --tag 2>/dev/null &>/dev/null 
+SCRIPTDIR=$(
+  cd $(dirname "$0")
+  pwd
+)
+
+if [ ! -f ${SCRIPTDIR}/luna.spec.in ]; then
+    echo "No luna.spec.in found"
+    exit 1
+fi
+
+if  ! git describe --tag 2>/dev/null &>/dev/null
 then
   VERSION=9999
   BUILD=$(git log --pretty=format:'' | wc -l)
@@ -10,10 +20,14 @@ else
   BUILD=$(git describe --tag  | sed -r 's/^v([\.0-9]*)-(.*)$/\2/' | tr - .)
 fi
 
-sed -e "s/__VERSION__/$VERSION/" rpm/luna.spec.in > rpm/luna.spec
-sed -i "s/__BUILD__/$BUILD/" rpm/luna.spec    
+mkdir -p ${SCRIPTDIR}/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
-git archive --format=tar.gz --prefix=luna-${VERSION}-${BUILD}/  -o ~/rpmbuild/SOURCES/v${VERSION}-${BUILD}.tar.gz HEAD
+sed -e "s/__VERSION__/$VERSION/" ${SCRIPTDIR}/luna.spec.in > ${SCRIPTDIR}/SPECS/luna.spec
+sed -i "s/__BUILD__/$BUILD/" ${SCRIPTDIR}/SPECS/luna.spec
 
-rpmbuild -bs rpm/luna.spec
-rm rpm/luna.spec
+git archive --format=tar.gz --prefix=luna-${VERSION}-${BUILD}/  -o ${SCRIPTDIR}/SOURCES/v${VERSION}-${BUILD}.tar.gz HEAD
+
+rm -rf ${SCRIPTDIR}/SRPMS/*.rpm
+rpmbuild -bs --define '_topdir ./rpm' ${SCRIPTDIR}/SPECS/luna.spec
+rm -rf ${SCRIPTDIR}/RPMS/*.rpm
+rpmbuild --rebuild --define "_topdir ${SCRIPTDIR}" ${SCRIPTDIR}/SRPMS/*.rpm
